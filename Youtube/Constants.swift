c605a5e0bcad662a213d978126baba410f230c18
Ivan Terziev <ivanterziev38@gmail.com>
2020-06-13 18:23:28 +0300

    Fixed the API not working by using a valid API Key. Getting the playlist, now works!

--- Youtube/Constants.swift
+++ Youtube/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
     
-    static var apiKey = "AIzaSyA6oKMZuSGaRyQXQ0Tw5H9O2AZOZVuVK5k"
+    static var apiKey = "AIzaSyCzpsCXtv6kMMx_tQcYlL3ith6WMiE5_og"
     static var playlistID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var apiURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.playlistID)&key=\(Constants.apiKey)"
     
